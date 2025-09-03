eef7ca7108ed40120058daab93c6001c37117b32
jl7512 <jimmyluu7512@hotmail.com>
2025-09-03 01:04:00 +0100

    Refactored and made Xbonacci solution less verbose and more concise

--- xbonacci/index.ts
+++ xbonacci/index.ts
@@ -1,27 +1,15 @@
-function nextValue(index, array) {
-  const copiedArray = [...array];
-  let value = 0;
-  for (index; index < array.length; index++) {
-    value += copiedArray[index];
-  }
-  return value;
-}
-
 // If array length is 4, sum the previous 4 values
 // Loop until array length equals n
-export function Xbonacci(signature, n) {
-  let result = signature.slice();
-
-  if (n <= result.length) {
-    return result.slice(0, n);
+export function Xbonacci(signature: number[], n: number) {
+  if (n <= signature.length) {
+    return signature.slice(0, n);
   } else {
     let index = 0;
-    for (let i = result.length; i < n; i++) {
-      const value = nextValue(index, result);
-      result.push(value);
+    for (let i = signature.length; i < n; i++) {
+      const value = signature.slice(index).reduce((total, currentVal) => total += currentVal);
+      signature.push(value);
       index++;
     }
   }
-  console.log(result.join(','));
-  return result.slice(0, n);
-}
+  return signature.slice(0, n);
+};
